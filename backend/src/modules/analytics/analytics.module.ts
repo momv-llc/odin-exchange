a929a18632e0a9e87a36d4def9d2cf8e560cf879
momv-llc <momv_llc@ukr.net>
2026-02-04 03:14:23 +0200

    Fix backend type checking and dependencies

--- backend/src/modules/analytics/analytics.module.ts
+++ backend/src/modules/analytics/analytics.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController, AdminAnalyticsController } from './analytics.controller';
-import { PrismaModule } from '../../prisma/prisma.module';
+import { PrismaModule } from '@/core/database/prisma.module';
 
 @Module({
   imports: [PrismaModule, ScheduleModule.forRoot()],
